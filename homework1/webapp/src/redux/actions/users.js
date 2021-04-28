8ae2c2c4042b85937d07d48fe8c36b58502f396b
Andrej-Novik <andrejnovikweb@gmail.com>
2021-04-28 09:49:19 +0300

    feature/P1-4: - adding thunks

--- homework1/webapp/src/redux/actions/users.js
+++ homework1/webapp/src/redux/actions/users.js
@@ -20,10 +20,10 @@ export function closeModal() {
 export function setCurrentUser(id, firstName, lastName) {
   return { type: SET_CURRENT_USER, id, firstName, lastName };
 }
-export function deleteUser(id) {
-  return { type: DELETE_USER, id };
+export function deleteStoreUser(userId) {
+  return { type: DELETE_USER, userId };
 }
-export function saveUser(id, firstName, lastName) {
+export function saveStoreUser(id, firstName, lastName) {
   if (id) {
     return { type: CHANGE_USER, id, firstName, lastName };
   } else {
@@ -37,19 +37,55 @@ export function userListError(value) {
   return { type: ERROR_GETTING_USERS, value };
 }
 export function userListSuccess(value) {
-  console.log(value);
-
   return { type: USER_LIST_SUCCESS, value };
 }
 
 export const getUsersList = () => async (dispatch) => {
-	dispatch(userLoader(true));
-	
-	const { value, error } = await Repository.APICore.getUsers();
-	
+  dispatch(userLoader(true));
+  const { value, error } = await Repository.APICore.getUsers();
   if (error || !value) {
     dispatch(userListError(true));
-	} else dispatch(userListSuccess(value));
-
+  } else dispatch(userListSuccess(value));
   dispatch(userLoader(false));
+};
+
+export const deleteUser = (userId) => async (dispatch) => {
+  const { value, error } = await Repository.APICore.deleteUser(userId);
+  if (error || !value) {
+    dispatch(userListError(true));
+  } else {
+    dispatch(deleteStoreUser(userId));
+  }
+};
+
+export const getUser = (userId) => async (dispatch) => {
+  const { value, error } = await Repository.APICore.getUser(userId);
+  if (error || !value) {
+    dispatch(userListError(true));
+  } else dispatch(userListSuccess(value));
+};
+
+export const saveUser = (userId, firstName, lastName) => async (dispatch) => {
+  if (userId) {
+    const { value, error } = await Repository.APICore.updateUser(
+      userId,
+      firstName,
+      lastName
+    );
+    if (error || !value) {
+      dispatch(userListError(true));
+    } else {
+      dispatch(saveStoreUser(userId, firstName, lastName));
+    }
+  } else {
+    const { value, error } = await Repository.APICore.createUser(
+      firstName,
+      lastName
+    );
+    if (error || !value) {
+      dispatch(userListError(true));
+    } else {
+      dispatch(saveStoreUser(userId, firstName, lastName));
+    }
+  }
 };
--- homework1/webapp/src/repository/core/index.js
+++ homework1/webapp/src/repository/core/index.js
@@ -14,5 +14,67 @@ class Core {
     }
     return result;
   };
+
+  deleteUser = async (userId) => {
+    const result = {
+      value: null,
+      error: null,
+    };
+    try {
+      const response = await axios.delete(`/api/users/${userId}`);
+      result.value = response.data;
+    } catch (e) {
+      result.error = e.response;
+    }
+    return result;
+  };
+
+  createUser = async (firstName, lastName) => {
+    const result = {
+      value: null,
+      error: null,
+    };
+    try {
+      const response = await axios.post(`/api/users/`, {
+        firstName,
+        lastName,
+      });
+      result.value = response.data;
+    } catch (e) {
+      result.error = e.response;
+    }
+    return result;
+  };
+
+  updateUser = async (userId, firstName, lastName) => {
+    const result = {
+      value: null,
+      error: null,
+    };
+    try {
+      const response = await axios.patch(`/api/users/${userId}`, {
+        firstName,
+        lastName,
+      });
+      result.value = response.data;
+    } catch (e) {
+      result.error = e.response;
+    }
+    return result;
+  };
+
+  getUser = async (userId) => {
+    const result = {
+      value: null,
+      error: null,
+    };
+    try {
+      const response = await axios.get(`/api/users/`, userId);
+      result.value = response.data;
+    } catch (e) {
+      result.error = e.response;
+    }
+    return result;
+  };
 }
 export default new Core();
